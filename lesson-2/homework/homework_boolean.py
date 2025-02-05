fc9b61cd3a6542c1c931f7877d3ca45845102f87
akbarevatov <a.evatov@newuu.uz>
2025-02-05 14:38:22 +0500

    Homework

---
+++ lesson-2/homework/homework_boolean.py
@@ -0,0 +1,45 @@
+# Q1
+u = input("Username: ")
+p = input("Password: ")
+if (u.strip()!="" and p.strip()!=""):
+    print("yes")
+
+# Q2
+a=input("Number1: ")
+b=input("Number2: ")
+if(a==b):
+    print("yes")
+
+# Q3
+n=input("Number: ")
+if(n%2==0 and n>0):
+    print("yes")
+
+# Q4
+n1=input("n1: ")
+n2=input("n2: ")
+n3=input("n3: ")
+if(n1!=n2 and n1!=n3 and n2!=n3):
+    print("yes")
+
+# Q5
+s1=input("s1: ")
+s2=input("s2: ")
+if(len(s1)==len(s2)):
+    print("yes")
+
+# Q6
+num=input("number: ")
+if(num%3==0 and num%5==0):
+    print("yes")
+
+# Q7
+num1=input("num1: ")
+num2=input("num2: ")
+if(num1+num2>50):
+    print("yes")
+
+# Q8
+number=input("Number 1: ")
+if(number>=10 and number<=20):
+    print("yes")
