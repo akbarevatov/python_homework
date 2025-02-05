b965e4e120543df49503098941673160bb619a8f
akbarevatov <a.evatov@newuu.uz>
2025-02-05 15:12:39 +0500

    Homework

--- lesson-2/homework/homework_boolean.py
+++ lesson-2/homework/homework_boolean.py
@@ -11,14 +11,14 @@ if(a==b):
     print("yes")
 
 # Q3
-n=input("Number: ")
+n=int(input("Number: "))
 if(n%2==0 and n>0):
     print("yes")
 
 # Q4
-n1=input("n1: ")
-n2=input("n2: ")
-n3=input("n3: ")
+n1=int(input("n1: "))
+n2=int(input("n2: "))
+n3=int(input("n3: "))
 if(n1!=n2 and n1!=n3 and n2!=n3):
     print("yes")
 
@@ -29,17 +29,17 @@ if(len(s1)==len(s2)):
     print("yes")
 
 # Q6
-num=input("number: ")
+num=int(input("number: "))
 if(num%3==0 and num%5==0):
     print("yes")
 
 # Q7
-num1=input("num1: ")
-num2=input("num2: ")
+num1=int(input("num1: "))
+num2=int(input("num2: "))
 if(num1+num2>50):
     print("yes")
 
 # Q8
-number=input("Number 1: ")
+number=int(input("Number 1: "))
 if(number>=10 and number<=20):
     print("yes")
--- lesson-2/homework/homework_numbers.py
+++ lesson-2/homework/homework_numbers.py
@@ -3,9 +3,9 @@ flt=float(input("float: "))
 print(round(flt,2))
 
 # Q2
-n1 = input("n1: ")
-n2 = input("n2: ")
-n3 = input("n3: ")
+n1 = int(input("n1: "))
+n2 = int(input("n2: "))
+n3 = int(input("n3: "))
 print("Smallest: ",min(n1,n2,n3))
 print("Largest: ",max(n1,n2,n3))
 
@@ -13,3 +13,23 @@ print("Largest: ",max(n1,n2,n3))
 km=float(input("km: "))
 m=km*1000
 print(f"{int(m)} meters and {int((m-int(m))*100)} centimeters")
+
+# Q4
+a=int(input("number 1: "))
+b=int(input("number 2: "))
+print(f"division={a//b} , remainder={a%b}")
+
+# Q5
+c=int(input("celsius: "))
+print(c*9/5+32)
+
+# Q6
+x=int(input("Number: "))
+print(x%10)
+
+# Q7
+y=int(input("Number: "))
+if(y%2==0):
+    print("yes")
+else:
+    print("no")
