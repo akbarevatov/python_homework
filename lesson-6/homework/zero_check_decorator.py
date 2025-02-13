1bc32c5978f0200da3a7dfa84c44fa00450c0fad
akbarevatov <a.evatov@newuu.uz>
2025-02-13 17:31:57 +0500

    homework

---
+++ lesson-6/homework/zero_check_decorator.py
@@ -0,0 +1,8 @@
+def check(func):
+    def wrapper(a, b):
+        if b==0: return "Denominator cannot be zero"
+        return func(a, b)
+    return wrapper
+@check
+def div(a, b):
+    return a / b
